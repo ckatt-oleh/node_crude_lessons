cc4bae683d888e8725b448f44a74c33bfff7f8cb
Hordiienko Oleh <hordienko.oleh@gmail.com>
2019-05-22 16:15:34 +0300

    fix list display

--- frontend/src/app/components/list/list.component.ts
+++ frontend/src/app/components/list/list.component.ts
@@ -1,45 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatTableDataSource } from '@angular/material';
 
 import { User } from '../../user.module';
 import { UserService } from '../../user.service';
 
+
+const ELEMENT_DATA: User[] = [];
+
+/**
+ * @title Basic use of `<table mat-table>`
+ */
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
 
-  users: User[];
-  displayedColums = ['name', 'email', 'username', 'password'];
+export class ListComponent implements OnInit {
+  
+  displayedColumns: string[] = ['username', 'name', 'email', 'actions'];
+  dataSource: any;
+
 
   constructor(private userService: UserService,
-    private router: Router) { }
+    private changeDetectorRefs: ChangeDetectorRef,
+    private router: Router) { 
 
-  ngOnInit() {
-    this.fetchUsers();
   }
 
-  fetchUsers() {
+  ngOnInit() {
+      this.refresh();
+  }
+
+  refresh() {
     this.userService
-      .getUsers()
-      .subscribe((data: User[]) => {
-        this.users = data;
-        console.log('Data requested ... ');
-        console.log(this.users);
-      });
+      .getUsers().subscribe((res: User[]) => {
+        this.dataSource = res;
+    });
   }
 
   editUser(id) {
     this.router.navigate([`/profile/${id}`]);
+    this.refresh();
   }
-
+    
   deleteUser(id) {
     this.userService.deleteUser(id).subscribe(() => {
-      this.fetchUsers();
+      this.refresh();
     });
   }
 
+
+
 }
+
+// export class ListComponent implements OnInit {
+  
+//   dataSource: MatTableDataSource<User>;
+//   users: User[] = [];
+//   displayedColums: string[] = ['username', 'name', 'email', 'actions'];
+
+//   constructor(private userService: UserService,
+//     private router: Router) { }
+
+//   ngOnInit() {
+//     this.fetchUsers();
+//   }
+
+//   fetchUsers() {
+//     this.userService
+//       .getUsers()
+//       .subscribe((data: User[]) => {
+//         this.users = data;
+//         console.log('Data requested ... ');
+//         this.dataSource = new MatTableDataSource(this.users);
+//       });
+//   }
+
+//  
+
+// }
+
+
